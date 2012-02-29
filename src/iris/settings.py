8293bb93d2c5a874a4e3b9b2911f347158d6d548
yuanye <deploy@verne.tvxio.com>
2012-02-29 17:56:40 +0800

    Add gunicorn

--- src/iris/settings.py
+++ src/iris/settings.py
@@ -56,16 +56,16 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
-
+STATIC_ROOT = (__file__.rpartition('/')[0] or '.') + '/static/'
+STATIC_BASE_URL = 'http://iris.tvxio.com'
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
-STATIC_URL = '/static/'
+STATIC_URL = STATIC_BASE_URL+'/static/'
 
 # URL prefix for admin static files -- CSS, JavaScript and images.
 # Make sure to use a trailing slash.
 # Examples: "http://foo.com/static/admin/", "/static/admin/".
-ADMIN_MEDIA_PREFIX = '/static/admin/'
+ADMIN_MEDIA_PREFIX = STATIC_BASE_URL+'/static/admin/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
