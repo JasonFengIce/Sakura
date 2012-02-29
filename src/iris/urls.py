215ae7b7fcbc7d55e523d3b1bc865073d2293d1b
elvis deng <dengtiecheng@ismartv.cn>
2012-02-29 16:48:46 +0800

    Change project name to iris

--- src/iris/urls.py
+++ src/iris/urls.py
@@ -7,7 +7,7 @@ admin.autodiscover()
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'service.views.home', name='home'),
-    url(r'^customer/', include('service.customer.urls')),
+    url(r'^customer/', include('iris.customer.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
