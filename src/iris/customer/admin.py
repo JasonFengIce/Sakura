877950b7818a18c921a16a6c9b95fbd40e01e46c
liuze <liuze2004@126.com>
2012-03-01 16:09:13 +0800

    creat date

--- src/iris/customer/admin.py
+++ src/iris/customer/admin.py
@@ -17,6 +17,8 @@ class PointlogAdmin(admin.ModelAdmin):
 class VideotypeAdmin(admin.ModelAdmin):
     list_display = ('type_name', 'bit_rate' , 'resolution',)
 
+    
+
 admin.site.register(models.Videotype,VideotypeAdmin)
 admin.site.register(models.Url,UrlAdmin)
 admin.site.register(models.Point,PointAdmin)
