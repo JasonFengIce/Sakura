f7262fbd4dd7707ab66c1573bfb572c88884b6c3
root <root@localhost.localdomain>
2012-05-22 17:11:03 +0800

    Modify gunicorn conf

--- src/iris/settings.py
+++ src/iris/settings.py
@@ -13,9 +13,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'iris',                      # Or path to database file if using sqlite3.
-        'USER': 'demo',                      # Not used with sqlite3.
-        'PASSWORD': 'demohello',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'USER': 'shiyuniris',                      # Not used with sqlite3.
+        'PASSWORD': '8GomD7txXunmNlZ9',                  # Not used with sqlite3.
+        'HOST': '10.0.3.4',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
