8cb31881b019ce131cebba82633f7bf10276c5d9
liuze <liuze@ismartv.cn>
2012-09-14 16:41:09 +0800

    Hant TW

--- src/iris/customer/views.py
+++ src/iris/customer/views.py
@@ -57,7 +57,9 @@ class Speedlogs(View):
                 return HttpResponse(videotype[0].en_resolution)
             if  'zh-TW'==type_temp:
                  return HttpResponse(videotype[0].hant_resolution)
-        
+
+        return HttpResponse(videotype[0].resolution)
+    
 class Pointlogs(View):
 
     def post(self,request):
