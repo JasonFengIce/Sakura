74ded5db588c0946eab5e946177f4d7a5ba1ffb2
Delacstrasz <delacstrasz@SONY-VAIO-KUbuntu.(none)>
2012-11-07 16:50:47 +0800

    add new A11 sakura

---
+++ app/SakuraForA11/src/cn/ismartv/speedtester/domain/SpeedInfoUploadEntity.java
@@ -0,0 +1,19 @@
+package cn.ismartv.speedtester.domain;
+
+public class SpeedInfoUploadEntity {
+	private String pk;
+	private float speed;
+	public String getPk() {
+		return pk;
+	}
+	public void setPk(String pk) {
+		this.pk = pk;
+	}
+	public float getSpeed() {
+		return speed;
+	}
+	public void setSpeed(float speed) {
+		this.speed = speed;
+	}
+	
+}
