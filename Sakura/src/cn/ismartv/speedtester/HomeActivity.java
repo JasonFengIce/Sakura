17dcac66b0f95b3dc385153c5f8aa15c727768e2
fenghuibin <fenghuibin@ismartv.cn>
2014-11-07 13:52:35 +0800

    fix null point

--- Sakura/src/cn/ismartv/speedtester/HomeActivity.java
+++ Sakura/src/cn/ismartv/speedtester/HomeActivity.java
@@ -3,7 +3,6 @@ package cn.ismartv.speedtester;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager;
-import android.util.Log;
 import butterknife.ButterKnife;
 import butterknife.InjectView;
 import cn.ismartv.speedtester.core.cache.CacheManager;
@@ -44,159 +43,15 @@ public class HomeActivity extends FragmentActivity {
 
     @Override
     public void onBackPressed() {
-        Log.d(TAG, "onBackPressed");
-
         FragmentSpeed fragmentSpeed = ((FragmentSpeed) tabAdapter.getSpeedFragment());
         DownloadTask downloadTask = fragmentSpeed.getDownloadTask();
-        Log.d(TAG, "down " + downloadTask.isRunning());
         if (null != downloadTask && downloadTask.isRunning()) {
             downloadTask.setRunning(false);
         } else {
-            if (fragmentSpeed.testProgressPopup.isShowing())
+            if (null != fragmentSpeed.testProgressPopup && fragmentSpeed.testProgressPopup.isShowing())
                 fragmentSpeed.testProgressPopup.dismiss();
             super.onBackPressed();
         }
     }
-
-    //
-//    @InjectView(R.id.speed_tab)
-//    ImageView speedTab;
-//
-//    @InjectView(R.id.feedback_tab)
-//    ImageView feedbackTab;
-//
-//    @InjectView(R.id.help_tab)
-//    ImageView helpTab;
-//
-//
-//    private FragmentSpeed fragmentSpeed;
-//    private FragmentFeedback fragmentFeedback;
-//    private FragmentHelp fragmentHelp;
-//
-//
-//    @Override
-//    public void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        Intent ootStartIntent = new Intent(this, HttpProxyService.class);
-//        this.startService(ootStartIntent);
-//
-//        setContentView(R.layout.main);
-//        ButterKnife.inject(this);
-//        initViews();
-//        fragmentSpeed = new FragmentSpeed();
-//        fragmentFeedback = new FragmentFeedback();
-//        fragmentHelp = new FragmentHelp();
-//        FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
-//        fragmentTransaction.setCustomAnimations(R.animator.fragment_slide_left_enter, R.animator.fragment_slide_left_exit,
-//                R.animator.fragment_slide_right_enter, R.animator.fragment_slide_right_exit
-//        );
-//        fragmentTransaction.replace(R.id.fragment, fragmentSpeed);
-//        fragmentTransaction.commit();
-//    }
-//
-//    public void initViews() {
-//        speedTab.setOnFocusChangeListener(this);
-//        feedbackTab.setOnFocusChangeListener(this);
-//        helpTab.setOnFocusChangeListener(this);
-//        helpTab.setOnClickListener(this);
-//        speedTab.setOnClickListener(this);
-//        feedbackTab.setOnClickListener(this);
-//    }
-//
-//    @Override
-//    public void onClick(View view) {
-//        switch (view.getId()) {
-//
-//            case R.id.speed_tab:
-//                FragmentTransaction fragmentTransaction1 = getFragmentManager().beginTransaction();
-//                fragmentTransaction1.setCustomAnimations(R.animator.fragment_slide_left_enter, R.animator.fragment_slide_left_exit,
-//                        R.animator.fragment_slide_right_enter, R.animator.fragment_slide_right_exit
-//                );
-//                fragmentTransaction1.replace(R.id.fragment, fragmentSpeed);
-//                fragmentTransaction1.commit();
-//
-//                break;
-//            case R.id.feedback_tab:
-//
-//                FragmentTransaction fragmentTransaction2 = getFragmentManager().beginTransaction();
-//                fragmentTransaction2.setCustomAnimations(R.animator.fragment_slide_left_enter, R.animator.fragment_slide_left_exit,
-//                        R.animator.fragment_slide_right_enter, R.animator.fragment_slide_right_exit
-//                );
-//                fragmentTransaction2.replace(R.id.fragment, fragmentFeedback);
-//                fragmentTransaction2.commit();
-//
-//                break;
-//            case R.id.help_tab:
-//                FragmentTransaction fragmentTransaction3 = getFragmentManager().beginTransaction();
-//                fragmentTransaction3.setCustomAnimations(R.animator.fragment_slide_left_enter, R.animator.fragment_slide_left_exit,
-//                        R.animator.fragment_slide_right_enter, R.animator.fragment_slide_right_exit
-//                );
-//
-//                fragmentTransaction3.replace(R.id.fragment, fragmentHelp);
-//                fragmentTransaction3.commit();
-//                break;
-//            default:
-//                break;
-//        }
-//
-//    }
-//
-//    @Override
-//    public void onFocusChange(View view, boolean focused) {
-//        if (focused) {
-//            AnimationSet animationSet = new AnimationSet(true);
-//            ScaleAnimation scaleAnimation = new ScaleAnimation(1, 2f, 1, 2f,
-//                    Animation.RELATIVE_TO_SELF, 0.5f,
-//                    Animation.RELATIVE_TO_SELF, 0.5f);
-//            scaleAnimation.setDuration(200);
-//            animationSet.addAnimation(scaleAnimation);
-//            animationSet.setFillAfter(true);
-//            view.startAnimation(animationSet);
-//            switch (view.getId()) {
-//
-//                case R.id.speed_tab:
-//                    FragmentTransaction fragmentTransaction1 = getFragmentManager().beginTransaction();
-//                    fragmentTransaction1.setCustomAnimations(R.animator.fragment_slide_left_enter, R.animator.fragment_slide_left_exit,
-//                            R.animator.fragment_slide_right_enter, R.animator.fragment_slide_right_exit
-//                    );
-//                    fragmentTransaction1.replace(R.id.fragment, fragmentSpeed);
-//                    fragmentTransaction1.commit();
-//
-//                    break;
-//                case R.id.feedback_tab:
-//
-//                    FragmentTransaction fragmentTransaction2 = getFragmentManager().beginTransaction();
-//                    fragmentTransaction2.setCustomAnimations(R.animator.fragment_slide_left_enter, R.animator.fragment_slide_left_exit,
-//                            R.animator.fragment_slide_right_enter, R.animator.fragment_slide_right_exit
-//                    );
-//                    fragmentTransaction2.replace(R.id.fragment, fragmentFeedback);
-//                    fragmentTransaction2.commit();
-//
-//                    break;
-//                case R.id.help_tab:
-//                    FragmentTransaction fragmentTransaction3 = getFragmentManager().beginTransaction();
-//                    fragmentTransaction3.setCustomAnimations(R.animator.fragment_slide_left_enter, R.animator.fragment_slide_left_exit,
-//                            R.animator.fragment_slide_right_enter, R.animator.fragment_slide_right_exit
-//                    );
-//
-//                    fragmentTransaction3.replace(R.id.fragment, fragmentHelp);
-//                    fragmentTransaction3.commit();
-//                    break;
-//                default:
-//                    break;
-//            }
-//        } else {
-//            AnimationSet animationSet = new AnimationSet(true);
-//            ScaleAnimation scaleAnimation = new ScaleAnimation(2, 1f, 2, 1f,
-//                    Animation.RELATIVE_TO_SELF, 0.5f,
-//                    Animation.RELATIVE_TO_SELF, 0.5f);
-//            scaleAnimation.setDuration(200);
-//            animationSet.addAnimation(scaleAnimation);
-//            animationSet.setFillAfter(true);
-//            view.startAnimation(animationSet);
-//        }
-//    }
-
-
 }
 
