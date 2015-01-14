6969043d424433f63e3430ff2a5f7bc2d342fcf8
fenghuibin <fenghuibin@ismartv.cn>
2015-01-14 17:22:00 +0800

    add preference package

---
+++ Sakura/src/cn/ismartv/preferences/PreferenceMode.java
@@ -0,0 +1,8 @@
+package cn.ismartv.preferences;
+
+/**
+ * Created by huaijie on 12/16/14.
+ */
+public enum PreferenceMode {
+    MODE_PRIVATE
+}
---
+++ Sakura/src/cn/ismartv/preferences/annotation/Preference.java
@@ -0,0 +1,20 @@
+package cn.ismartv.preferences.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Created by huaijie on 11/17/14.
+ */
+
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Preference {
+    public String name() default "";
+
+
+
+}
